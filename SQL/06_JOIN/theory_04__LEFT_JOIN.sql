a74a61cdea7afa5d0d154a8432a003834ae66685
Katarina-dev <sadalsud8@gmail.com>
2024-09-27 12:08:44 +0200

    270924

--- SQL/06_JOIN/theory_04__LEFT_JOIN.sql
+++ SQL/06_JOIN/theory_04__LEFT_JOIN.sql
@@ -2,7 +2,6 @@
 даже если нет соответствий в правой таблице (departments).
 Если соответствий нет, возвращаются NULL */
 
-USE 050824_learning_join_command;
 
 SELECT * FROM employees;
 SELECT * FROM departments;
