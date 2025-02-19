714ca4a55c9aab2595c859e893bc5498f27afacf
Katarina-dev <sadalsud8@gmail.com>
2025-02-19 23:49:06 +0100

    add DA_hw_3

--- Python/Algorithms/alg_1.py
+++ Python/Algorithms/alg_1.py
@@ -1,6 +1,6 @@
-num1, num2, num3 = map(int, input('Input three numbers: ').split())
-sum = num1 + num2 + num3
-print(sum)
+# num1, num2, num3 = map(int, input('Input three numbers: ').split())
+# sum = num1 + num2 + num3
+# print(sum)
 
 # ALGORITHM Find sum of three numbers
 #
@@ -10,4 +10,8 @@ print(sum)
 # WRITE: sum = num1 + num2 + num3
 # END
 
+a = [1, 2, 3]
+b = [4, 5, 6]
 
+sum = [i + j for i, j in zip(a, b)]
+print(sum)
